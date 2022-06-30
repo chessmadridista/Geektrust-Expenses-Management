71f4690298a2ad35b66c82675b561d729bda9386
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-06-30 14:13:57 +0530

    Add functions to calculate debt owed and total expenditure.

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ class ExpensesCalculator:
   def __init__(self):
     self.__number_of_people = 0
     self.__people = list()
+    self.__total_expenditure = dict()
+    self.__amount_loaned = dict()
   
-  def __process_move_in(self, words_in_input_command):
+  def __set_move_in(self, words_in_input_command):
     name = words_in_input_command[1]
 
     if self.__number_of_people < self.__max_number_of_people:
@@ -19,7 +21,7 @@ class ExpensesCalculator:
     
     return message
   
-  def __process_move_out(self, words_in_input_command):
+  def __set_move_out(self, words_in_input_command):
     name = words_in_input_command[1]
 
     if self.__number_of_people > self.__number_of_people_in_empty_house:
@@ -30,7 +32,7 @@ class ExpensesCalculator:
     
     return message
   
-  def check_member_existence(self, members):
+  def __get_member_existence(self, members):
     is_everyone_a_member = True
     
     for member in members:
@@ -41,26 +43,56 @@ class ExpensesCalculator:
         break
     
     return is_everyone_a_member
+  
+  def __set_total_expenditure(self, spend_amount, members):
+    member_owed = members[0]
+    no_of_members = len(members)
+    spend_amount //= no_of_members
 
-  def __process_spend(self, words_in_input_command):
+    for member in members:
+      if member in self.__total_expenditure:
+        self.__total_expenditure[member] += spend_amount
+      else:
+        self.__total_expenditure[member] = spend_amount
+
+    return True
+  
+  def __set_member_loans(self, spend_amount, member_owed, members):
+    for member in members[1:]:
+      if member in self.__amount_loaned[member_owed]:
+        self.__amount_loaned[member_owed][member] += spend_amount
+      else:
+        self.__amount_loaned[member_owed][member] = spend_amount
+    
+    return True
+  
+  def __set_amount_loaned(self, spend_amount, members):
+    member_owed = members[0]
+    
+    if member_owed in self.__amount_loaned:
+      pass
+    else:
+      self.__amount_loaned[member_owed] = dict()
+    
+    self.__set_member_loans(spend_amount, member_owed, members)
+
+    return True
+
+  def __set_spend(self, words_in_input_command):
     spend_amount = int(words_in_input_command[1])
     members = words_in_input_command[2:]
-    is_everyone_a_member = check_member_existence(members)
+    is_everyone_a_member = self.__get_member_existence(members)
 
     if is_everyone_a_member:
-      pass
+      self.__set_total_expenditure(spend_amount, members)
+      self.__set_amount_loaned(spend_amount, members)
+      message = "SUCCESS"
     else:
       message = "MEMBER_NOT_FOUND"
 
-    if self.__number_of_people > self.__number_of_people_in_empty_house:
-      self.__number_of_people -= 1
-      message = "SUCCESS"
-    else:
-      message = "HOUSEFUL"
-    
     return message
 
-  def __process_dues(self, words_in_input_command):
+  def __get_dues(self, words_in_input_command):
     name = words_in_input_command[1]
 
     if self.__number_of_people > self.__number_of_people_in_empty_house:
@@ -71,7 +103,7 @@ class ExpensesCalculator:
     
     return message
 
-  def __process_clear_due(self, words_in_input_command):
+  def __set_clear_due(self, words_in_input_command):
     name = words_in_input_command[1]
 
     if self.__number_of_people > self.__number_of_people_in_empty_house:
@@ -82,12 +114,12 @@ class ExpensesCalculator:
     
     return message
 
-  def __process_invalid_command(self):
+  def __get_invalid_command(self):
     error_message = "The command is not valid! Please try again with a different command."
 
     return error_message
   
-  def __process_command(self, input_command):
+  def __set_command(self, input_command):
     words_in_input_command = input_command.split()
     command = words_in_input_command[0]
     command_move_in = "MOVE_IN"
@@ -112,7 +144,7 @@ class ExpensesCalculator:
     return message
   
   def main(self, input_command):
-    message = self.__process_command(input_command)
+    message = self.__set_command(input_command)
 
     return message
 
