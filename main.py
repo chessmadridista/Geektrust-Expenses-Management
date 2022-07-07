51a4ed84a3f4631412e8e97d008ed2f8a731d4d6
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-07-07 19:50:44 +0530

    Restructure codebase to keep set_move_out function at the bottom part of the applications for better readability.

--- main.py
+++ main.py
@@ -21,33 +21,7 @@ class ExpensesCalculator:
     
     return message
   
-  def __has_dues(self, words_in_input_command):
-    are_dues_clear = True
-    dues = self.__get_dues(words_in_input_command)
-
-    for due in dues:
-      due_amount = due.split()
-
-      if due_amount == "0":
-        continue
-      else:
-        are_dues_clear = False
-        break
-    
-    return are_dues_clear
   
-  def __set_move_out(self, words_in_input_command):
-    name = words_in_input_command[1]
-    are_paid_dues_clear = self.__has_dues(words_in_input_command)
-    are_received_dues_clear = self.__has_dues(words_in_input_command)
-
-    if self.__number_of_people > self.__number_of_people_in_empty_house:
-      self.__number_of_people -= 1
-      message = "SUCCESS"
-    else:
-      message = "HOUSEFUL"
-    
-    return message
   
   def __get_member_existence(self, members):
     is_everyone_a_member = True
@@ -122,6 +96,34 @@ class ExpensesCalculator:
 
   def __set_clear_due(self, words_in_input_command):
     name = words_in_input_command[1]
+
+    if self.__number_of_people > self.__number_of_people_in_empty_house:
+      self.__number_of_people -= 1
+      message = "SUCCESS"
+    else:
+      message = "HOUSEFUL"
+    
+    return message
+  
+  def __has_dues(self, words_in_input_command):
+    are_dues_clear = True
+    dues = self.__get_dues(words_in_input_command)
+
+    for due in dues:
+      due_amount = due.split()
+
+      if due_amount == "0":
+        continue
+      else:
+        are_dues_clear = False
+        break
+    
+    return are_dues_clear
+  
+  def __set_move_out(self, words_in_input_command):
+    name = words_in_input_command[1]
+    are_paid_dues_clear = self.__has_dues(words_in_input_command)
+    are_received_dues_clear = self.__has_dues(words_in_input_command)
 
     if self.__number_of_people > self.__number_of_people_in_empty_house:
       self.__number_of_people -= 1
