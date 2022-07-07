3b307e1c13df94c7a89785d53dccafa6170b6cc6
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-07-07 19:37:55 +0530

    Add function to check whether the dues paid to a person have been cleared or not.

--- main.py
+++ main.py
@@ -21,8 +21,25 @@ class ExpensesCalculator:
     
     return message
   
+  def __has_dues(self, words_in_input_command):
+    are_dues_clear = True
+    dues = self.__get_dues(words_in_input_command)
+
+    for due in dues:
+      due_amount = due.split()
+
+      if due_amount == "0":
+        continue
+      else:
+        are_dues_clear = False
+        break
+    
+    return are_dues_clear
+  
   def __set_move_out(self, words_in_input_command):
     name = words_in_input_command[1]
+    are_paid_dues_clear = self.__has_dues(words_in_input_command)
+    are_received_dues_clear = self.__has_dues(words_in_input_command)
 
     if self.__number_of_people > self.__number_of_people_in_empty_house:
       self.__number_of_people -= 1
