9e6ca2960a88252e2a6147b4eb5297a76fd144c2
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-07-13 11:32:45 +0530

    Move functions related to 'move_out' command to its respective module.

--- main.py
+++ main.py
@@ -110,18 +110,7 @@ class ExpensesCalculator:
     
     return are_dues_clear
   
-  def __set_move_out(self, words_in_input_command):
-    name = words_in_input_command[1]
-    are_paid_dues_clear = self.__has_dues(words_in_input_command)
-    are_received_dues_clear = self.__has_dues(words_in_input_command)
-
-    if self.__number_of_people > self.__number_of_people_in_empty_house:
-      self.__number_of_people -= 1
-      message = "SUCCESS"
-    else:
-      message = "HOUSEFUL"
-    
-    return message
+  
 
   def __get_invalid_command(self):
     error_message = "The command is not valid! Please try again with a different command."
