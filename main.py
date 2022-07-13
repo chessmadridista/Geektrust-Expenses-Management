e99c09c11b312523b0b9471bda0c0fd1a8f29e81
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-07-13 11:35:13 +0530

    Move functions related to 'spend' command to its respective module.

--- main.py
+++ main.py
@@ -59,19 +59,7 @@ class ExpensesCalculator:
 
     return True
 
-  def __set_spend(self, words_in_input_command):
-    spend_amount = int(words_in_input_command[1])
-    members = words_in_input_command[2:]
-    is_everyone_a_member = self.__get_member_existence(members)
-
-    if is_everyone_a_member:
-      self.__set_total_expenditure(spend_amount, members)
-      self.__set_amount_loaned(spend_amount, members)
-      message = "SUCCESS"
-    else:
-      message = "MEMBER_NOT_FOUND"
-
-    return message
+  
 
   def __get_dues(self, words_in_input_command):
     name = words_in_input_command[1]
--- operations/spend.py
+++ operations/spend.py
@@ -1,3 +1,17 @@
 class Spend:
   def __init__(self):
     pass
+
+  def __set_spend(self, words_in_input_command):
+    spend_amount = int(words_in_input_command[1])
+    members = words_in_input_command[2:]
+    is_everyone_a_member = self.__get_member_existence(members)
+
+    if is_everyone_a_member:
+      self.__set_total_expenditure(spend_amount, members)
+      self.__set_amount_loaned(spend_amount, members)
+      message = "SUCCESS"
+    else:
+      message = "MEMBER_NOT_FOUND"
+
+    return message
