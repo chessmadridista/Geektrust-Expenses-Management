10aadec4040b97e6039711496d7379c013a01cec
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-07-27 00:59:11 +0530

    Change single underscore to double underscore in case of module level privacy of functions.

--- operations/clear_due.py
+++ operations/clear_due.py
@@ -1,28 +1,25 @@
-class ClearDue:
-  @staticmethod
-  def __has_dues(self, words_in_input_command):
-    are_dues_clear = True
-    dues = self.__get_dues(words_in_input_command)
+def _has_dues(words_in_input_command):
+  are_dues_clear = True
+  dues = self.__get_dues(words_in_input_command)
 
-    for due in dues:
-      due_amount = due.split()
+  for due in dues:
+    due_amount = due.split()
 
-      if due_amount == "0":
-        continue
-      else:
-        are_dues_clear = False
-        break
-    
-    return are_dues_clear
-
-  @staticmethod
-  def set_clear_due(self, words_in_input_command):
-    name = words_in_input_command[1]
-
-    if self.__number_of_people > self.__number_of_people_in_empty_house:
-      self.__number_of_people -= 1
-      message = "SUCCESS"
+    if due_amount == "0":
+      continue
     else:
-      message = "HOUSEFUL"
-    
-    return message
+      are_dues_clear = False
+      break
+  
+  return are_dues_clear
+
+def set_clear_due(words_in_input_command):
+  name = words_in_input_command[1]
+
+  if self.__number_of_people > self.__number_of_people_in_empty_house:
+    self.__number_of_people -= 1
+    message = "SUCCESS"
+  else:
+    message = "HOUSEFUL"
+  
+  return message
